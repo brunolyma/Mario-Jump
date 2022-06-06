7aa59f67598fc3d9bb3ac534343a9b7fd90e4236
Bruno Lima <brunoadl88@gmail.com>
2022-06-06 16:46:25 -0300

    score-board

--- js/script.js
+++ js/script.js
@@ -1,10 +1,18 @@
 const mario = document.querySelector(".mario")
 const pipe = document.querySelector(".pipe")
 const clouds = document.querySelector(".clouds")
+const scoreCounter = document.querySelector('.score')
 let gameOver = false
+let score = 0
 
 document.addEventListener("keypress", start)
 document.addEventListener("touchstart", start)
+
+function scoreBoard () {
+  
+  score += 10
+  scoreCounter.innerText  = `score ${("000000" + score).slice(-6)}`
+}
 
 function start(event) {
     if (gameOver == false) {
@@ -22,11 +30,9 @@ const loop = setInterval(() => {
     window.getComputedStyle(mario).bottom.replace("px", "")
   ).toFixed()
   if (jumped < 70 && pipe.offsetLeft < 125) {
-    console.log("game-over")
+    console.log("GAME-OVER")
     gameover(clouds.offsetLeft, pipe.offsetLeft)
     clearInterval(loop)
-  } else {
-    console.log("pulou")
   }
 }, 50)
 
@@ -47,4 +53,5 @@ const jump = () => {
 const run = () =>
   setTimeout(() => {
     mario.classList.remove("jump")
+    scoreBoard()
   }, 1000)
